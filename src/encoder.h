926546429fc913a06aac642ce6089fdae42c11d0
Hesamedine Jannesari <hesamverse@proton.me>
2025-05-12 13:23:36 +0200

    Add encode_bool to encoder.h

--- src/encoder.h
+++ src/encoder.h
@@ -10,5 +10,6 @@ typedef struct {
 FunctionSignature parse_function_signature(const char *signature);
 char *encode_address(const char *value);
 char *encode_uint256(const char *value);
+char *encode_bool(const char *value);
 
 #endif
