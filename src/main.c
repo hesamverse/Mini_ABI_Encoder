7dbb7b24cb48621c559c643baf620a9cba5bae08
Hesamedine Jannesari <hesamverse@proton.me>
2025-05-12 11:32:59 +0200

    Edit main.c to support address and uint256

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "encoder.h"
 #include "utils.h"
 
@@ -16,6 +18,24 @@ int main(int argc, char *argv[]) {
         printf(" %s", input.params[i]);
     }
     printf("\n");
+
+    for (int i = 0; i < fsig.param_count; i++) {
+        const char *type = fsig.param_types[i];
+        const char *value = input.params[i];
+        char *encoded = NULL;
+
+        if (strcmp(type, "address") == 0) {
+            encoded = encode_address(value);
+        } else if (strcmp(type, "uint256") == 0) {
+            encoded = encode_uint256(value);
+        } else {
+            fprintf(stderr, "Unsupported type: %s\n", type);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Encoded param %d (%s): %s\n", i + 1, type, encoded);
+        free(encoded);
+    }
 
     return 0;
 }
