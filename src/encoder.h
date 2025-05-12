bb2f6a950a3b3a8376873863ced6a5bd544af24e
Hesamedine Jannesari <hesamverse@proton.me>
2025-05-12 11:31:19 +0200

    Edit encoder.h to support address and uint256

--- src/encoder.h
+++ src/encoder.h
@@ -8,7 +8,6 @@ typedef struct {
 } FunctionSignature;
 
 FunctionSignature parse_function_signature(const char *signature);
-
 char *encode_address(const char *value);
 char *encode_uint256(const char *value);
 
