619e82bfd665bef74cf7628735891c6a834522c3
Hesamedine Jannesari <hesamverse@proton.me>
2025-05-12 18:30:33 +0200

    Writing the file test_vectors.c

--- tests/test_vectors.c
+++ tests/test_vectors.c
@@ -1,3 +1,12 @@
-// for file in tests/json/*.json; do
-//   ./abi_encoder --json-file "$file"
-// done
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int result = system("./abi_encoder --json-file tests/json/transfer.json");
+    if (result != 0) {
+        fprintf(stderr, "Test failed: transfer.json\n");
+        return 1;
+    }
+    printf("Test passed: transfer.json\n");
+    return 0;
+}
