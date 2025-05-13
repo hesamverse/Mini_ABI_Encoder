b86c108a32f9704555dd49db9d205ebc9db1c9c2
Hesamedine Jannesari <hesamverse@proton.me>
2025-05-13 12:38:56 +0200

    Using #include utils.h instead of #include encoder.h

--- include/cli.h
+++ include/cli.h
@@ -1,10 +1,23 @@
+// cli.h
+// --------------------------------------------------
+// Command-Line Argument Parser for Mini ABI Encoder
+// --------------------------------------------------
+// Author: Hesamverse
+// Description:
+//     Declares the functions used to parse CLI arguments
+//     into usable data structures for encoding.
+//
+//     Depends on CLIInput struct defined in utils.h.
+//
 #ifndef CLI_H
 #define CLI_H
 
-#include "encoder.h"
+#include "utils.h"  // Needed for CLIInput type
 
-void print_help();
+// Prints full help message to stdout
+void print_help(void);
 
+// Parses full CLI argument set into a CLIInput structure
 CLIInput parse_cli_args_full(int argc, char *argv[], int *only_selector, int *pretty, int *json_out, int *interactive);
 
-#endif
+#endif // CLI_H
